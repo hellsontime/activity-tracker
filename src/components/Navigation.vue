ee6ec09fe8bfe3adf94d49e7bc8d6475c13211eb
hellsontime <roma67177@gmail.com>
2022-07-15 17:14:55 +0300

    add logo router-link + create route

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="bg-at-light-green text-white">
     <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
-      <div class="flex items-center gap-x-4">
+      <router-link class="cursor-pointer flex items-center gap-x-4" :to="{name: 'Home'}">
         <img class="w-14" src="../assets/images/dumbbell-light.png" alt="logo">
         <h1 class="text-lg">
           Activity Tracker
         </h1>
-      </div>
+      </router-link>
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link class="cursor-pointer" :to="{name: 'Home'}">Home</router-link>
-        <router-link v-if="user" class="cursor-pointer" :to="{name: ''}">Create</router-link>
+        <router-link v-if="user" class="cursor-pointer" :to="{name: 'Create'}">Create</router-link>
         <router-link v-if="!user" class="cursor-pointer" :to="{name: 'Login'}">Login</router-link>
         <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
       </ul>
